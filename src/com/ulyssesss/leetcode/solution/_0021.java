892930d0d6cbf772fec0132bb24323bc40e30502
jiangyue <465100566@qq.com>
2020-01-15 15:33:18 +0800

    21

---
+++ src/com/ulyssesss/leetcode/solution/_0021.java
@@ -0,0 +1,34 @@
+package com.ulyssesss.leetcode.solution;
+
+import com.ulyssesss.leetcode.common.ListNode;
+
+/**
+ * 21. 合并两个有序链表
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ */
+public class _0021 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        current.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+}
