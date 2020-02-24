2eac0f16396e45dfe05b8102150b6e75c90e7fc6
jiangyue <465100566@qq.com>
2020-02-24 20:38:21 +0800

    107

---
+++ src/com/ulyssesss/leetcode/solution/_0107.java
@@ -0,0 +1,89 @@
+package com.ulyssesss.leetcode.solution;
+
+import com.ulyssesss.leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 107. 二叉树的层次遍历 II
+ *
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其自底向上的层次遍历为：
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ */
+public class _0107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                root = queue.remove();
+                list.add(root.val);
+                if (root.left != null) {
+                    queue.add(root.left);
+                }
+                if (root.right != null) {
+                    queue.add(root.right);
+                }
+            }
+            result.add(0, list);
+        }
+        return result;
+    }
+
+    public List<List<Integer>> _levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        addList(result, queue);
+        return result;
+    }
+
+    private void addList(List<List<Integer>> result, Queue<TreeNode> queue) {
+        if (queue.isEmpty()) {
+            return;
+        }
+        int size = queue.size();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            TreeNode node = queue.remove();
+            list.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        result.add(0, list);
+        addList(result, queue);
+    }
+}
