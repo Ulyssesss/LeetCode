a0a2662394037611f017c4f3bb65275f740a9a79
jiangyue <465100566@qq.com>
2020-03-12 19:48:36 +0800

    110

--- src/com/ulyssesss/leetcode/common/TreeNode.java
+++ src/com/ulyssesss/leetcode/common/TreeNode.java
@@ -1,10 +1,30 @@
 package com.ulyssesss.leetcode.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TreeNode {
 
     public int val;
     public TreeNode left;
     public TreeNode right;
+
+    public static TreeNode createNodes(Integer[] array) {
+        Map<Integer, TreeNode> nodes = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                nodes.put(i, new TreeNode(array[i]));
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            TreeNode node = nodes.get(i);
+            if (node != null) {
+                node.left = nodes.get(i * 2 + 1);
+                node.right = nodes.get(i * 2 + 2);
+            }
+        }
+        return nodes.get(0);
+    }
 
     public TreeNode(int x) {
         this.val = x;
---
+++ src/com/ulyssesss/leetcode/solution/_0110.java
@@ -0,0 +1,77 @@
+package com.ulyssesss.leetcode.solution;
+
+import com.ulyssesss.leetcode.common.TreeNode;
+
+/**
+ * 110. 平衡二叉树
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ *
+ * 本题中，一棵高度平衡二叉树定义为：
+ *
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ *
+ * 示例 1:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回 true 。
+ *
+ *
+ * 示例 2:
+ *
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ *
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * 返回 false 。
+ *
+ */
+public class _0110 {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(depth(root.left) - depth(root.right)) < 2;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+    public boolean _isBalanced(TreeNode root) {
+        return _depth(root) != -1;
+    }
+
+    private int _depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = _depth(root.left);
+        if (left == -1) {
+            return -1;
+        }
+        int right = _depth(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
